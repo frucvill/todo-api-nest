e9127f5866f6ced5ece7d5d6288427c6d983bcc1
frucvill <fructuoso.villarroel@gmail.com>
2026-07-21 19:01:46 -0400

    ADD SWAGGER

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       transform: true, // transform the payload to the DTO class
     }),
   );
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('TODOs API')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .addTag('todos') // add path prefix for all endpoints in this tag
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
+  @ApiProperty({
+    description: 'The title of the todo',
+    example: 'My first todo',
+  })
   @IsString()
   @IsNotEmpty()
   readonly title!: string;
